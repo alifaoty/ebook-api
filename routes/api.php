8496718b3232456f1d5cae6b38af1d57e542a0b9
alifaotys <alifaotysalsabilla@gmail.com>
2022-10-27 18:28:08 +0700

    second commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    //return $request->user();
+//});
+
+Route::get('hello', function(){
+    return ["me" => "Hello, it's me Marcello Este"];
+}); 
+
+Route::resource('hellocello', HeloController::class);
+
+Route::resource('siswa', SiswaController::class);
+
+// Route::get('books', [BookController::class, 'index']);
+// Route::get('books/{id}', [BookController::class, 'show']);
+// Route::get('books', [BookController::class, 'store']);
+// Route::get('books/{id}', [BookController::class, 'update']);
+// Route::get('books/{id}', [AuthController::class, 'destroy']);
+
+Route::resource('books', BookController::class)->except(
+    ['create','edit']
+);
